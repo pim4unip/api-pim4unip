db80d2ae4b3225b0209518d1193bdf2403a2aee7
marcosrensi <marquinhos_rensi@hotmail.com>
2021-11-24 09:26:33 -0300

    teste

--- routes/acomodacoes.js
+++ routes/acomodacoes.js
@@ -62,20 +62,25 @@ router.get('/listar-disponiveis', login.optional, (req, res, next) => {
 
 // RESERVAS
 
-router.post('/reserva', login.optional, (req, res, next) => {    
+// INSERIR USUARIO
+router.post('/reserva/', (req, res, next) => {
     mysql.getConnection((error, conn) => {
         if (error) { return res.status(500).send({ error: error }) }
-        conn.query(
-            'call buscarCalendario (5, 5, 95, "24/11/2021", "26/11/2021", 2, 100, 200, "26/11/2021", 5,"26/11/2021", 5)',
-            (error, resultado, field) => {
-                conn.release();
-                if (error) { return res.status(500).send({ error: error }) }
-                return res.status(201).send({
-                    response: resultado //resultado
-                })
-            }
-        )
-    })
+                    conn.query(
+                        'call buscarCalendario (5, 5, 95, "24/11/2021", "26/11/2021", 2, 100, 200, "26/11/2021", 5,"26/11/2021", 5)',
+                        //[req.body.usuario, hash, req.body.nome, req.body.data],
+                        (error, resultado, field) => {
+                            conn.release();
+                            if (error) { return res.status(500).send({ error: error }) }
+                            res.status(201).send({
+                                mensagem: 'Cadastrado com sucesso'//,
+                                //id: resultado.insertId,
+                                //email: req.body.usuario
+                            })
+                        }
+                    )
+
+    });
 });
 
 module.exports = router;
